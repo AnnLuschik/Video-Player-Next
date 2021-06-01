6e9983cc362e203bab1aac8897a3468363736aee
Hanna Lushchyk <hanna.lushchyk@itrexgroup.lan>
2021-06-01 17:34:41 +0300

    test

--- src/auth/LoginButton/LoginButton.jsx
+++ src/auth/LoginButton/LoginButton.jsx
@@ -5,7 +5,14 @@ import { useAuth0 } from '@auth0/auth0-react';
 export const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
 
-  return <StyledButton onClick={() => loginWithRedirect()}>Log in</StyledButton>
+  return (
+    <StyledButton onClick={() => loginWithRedirect({
+      redirect_uri: 'https://annluschik.github.io/Video-Player/',
+    })}
+    >
+      Log in
+    </StyledButton>
+  )
 }
 
 const StyledButton = styled.button`
