0965ece2c67364fa05de7fa64260ae8716e897fd
Hanna Lushchyk <hanna.lushchyk@itrexgroup.com>
2021-08-17 12:25:50 +0300

    config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 const withImages = require('next-images');
 
+const isProd = process.env.NODE_ENV === 'production';
+
 module.exports = withImages({
+  assetPrefix: isProd ? '/Video-Player-Next/' : '',
   esModule: true,
   webpack(config) {
     return config
