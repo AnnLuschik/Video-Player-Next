219013b84cacb36f7b756b70935e14b8fd2780a7
AnnLuschik <66325947+AnnLuschik@users.noreply.github.com>
2021-08-23 09:45:16 +0000

    Deploying to gh-pages from @ AnnLuschik/Video-Player-Next@ea5d53354d1e8648f26ca9c622888c209692f402 🚀

--- _next/static/Fw6Az9xGspXFIoYnKDoGx/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/681-40d64679c9c37682203f.js","static/chunks/pages/index-3a58e8546c31ec80ae56.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
