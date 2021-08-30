2b69f8a83843e0fe707bedd4dd7fb9c13e96daf4
Hanna Lushchyk <hanna.lushchyk@itrexgroup.com>
2021-08-30 13:46:38 +0300

    test: correct firestore import

--- firebase/clientApp.ts
+++ firebase/clientApp.ts
@@ -1,5 +1,5 @@
 import firebase from 'firebase/app';
-import 'firebase/firebase-firestore';
+import 'firebase/firestore';
 import 'firebase/storage';
 
 const firebaseConfig = {
@@ -11,7 +11,7 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-if (!firebase.apps.length) {
+if (typeof window !== 'undefined' && !firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 }
 
